9f56824326cd91ac4b3000c66399f4d11e11922f
Marcus Rådell <marcus.radell@wetal.io>
2019-12-10 15:27:24 +0100

    add props to profile

--- web/src/components/profile/index.stories.tsx
+++ web/src/components/profile/index.stories.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
 import { Button } from "@storybook/react/demo";
-import { Profile } from ".";
+import { ProfileFactory } from ".";
 
-const profile = Profile();
+const profile = ProfileFactory({
+  title: "Web developer",
+  name: "Marcus Rådell",
+  imageUrl:
+    "https://pbs.twimg.com/profile_images/1182702044395778049/KT2I-JrX_400x400.jpg"
+});
 
 export default {
   title: "profile",
--- web/src/components/profile/index.tsx
+++ web/src/components/profile/index.tsx
@@ -1,7 +1,19 @@
 import React, { FC } from "react";
 
-export const Profile = () => {
-  const view: FC<unknown> = () => <>profile</>;
+export type ProfileFactoryProps = {
+  name: string;
+  title: string;
+  imageUrl: string;
+};
+
+export const ProfileFactory = (p: ProfileFactoryProps) => {
+  const view: FC<unknown> = () => (
+    <>
+      <h1>{p.title}</h1>
+      <img src={p.imageUrl} alt="profile image" />
+      <h2>{p.name}</h2>
+    </>
+  );
 
   return {
     view
